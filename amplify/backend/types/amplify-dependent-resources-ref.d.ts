5a2b2e77575f1c999769885ec71952632171cb36
TheGodojo <godot.dojo@gmail.com>
2022-12-06 00:10:51 -0500

    added basic elements

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "tracky97147d07": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "trackyStorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Wake Time</h1>
+        <input name="" type="time">
+          
+        </input>
+        <h1>Sleep Time</h1>
+        <input name="" type="time">
+          
+        </input>
+        <h1>Bed Time</h1>
+        <input name="" type="time">
+          
+        </input>
+      </div>
     </div>
   );
 }
